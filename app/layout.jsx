7993de4f04e30a7345b6f97ed17aeb411a555dba
Trickster <trickster_mel@Tricksters-MacBook-Pro-2.local>
2025-10-24 11:23:50 +0300

    speed insights

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
